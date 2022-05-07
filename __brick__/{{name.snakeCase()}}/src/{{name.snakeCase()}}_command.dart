fb39b97dadfc621db90b6f7110e7134044bbaaf0
Prashant Nigam <prashantnigam490@gmail.com>
2022-05-07 16:20:57 +0530

    Initial commit

---
+++ __brick__/{{name.snakeCase()}}/src/{{name.snakeCase()}}_command.dart
@@ -0,0 +1,34 @@
+import 'package:args/args.dart';
+import 'package:args/command_runner.dart';
+import 'package:mason_logger/mason_logger.dart';
+
+/// Command for the module.
+class {{name.pascalCase()}}Command extends Command<int> {
+  /// Constructor.
+  /// {@macro easy_app_cli}
+  {{name.pascalCase()}}Command({
+    Logger? logger,
+  })  : _logger = logger ?? Logger(),
+        super();
+
+  final Logger _logger;
+
+  @override
+  String get description => '{{description}}';
+
+  @override
+  String get name => '{{name.snakeCase()}}';
+
+  @override
+  // TODO: implement name
+  String get invocation => throw UnimplementedError();
+
+  @override
+  // TODO: implement name
+  String get summary => '$invocation\n$description';
+
+  /// [ArgResults] which can be overridden for testing.
+  ArgResults? argResultOverrides;
+
+  ArgResults get _argResults => argResultOverrides ?? argResults!;
+}
---
+++ __brick__/{{name.snakeCase()}}/{{name.snakeCase()}}.dart
@@ -0,0 +1,1 @@
+export 'src/{{name.snakeCase()}}_command.dart';
